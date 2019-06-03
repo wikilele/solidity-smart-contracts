0e9b5ce0cfc351ed48207c362453fb1e778e3a7e
leonardo <l.frioli@studenti.unipi.it>
2019-06-03 19:42:14 +0200

    Only need to test the contract and to check the gas.

--- VickeryAuction.sol
+++ VickeryAuction.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.22;
 
 // the contract itself will act as the third party auction house
 contract VickeryAuction{
@@ -6,91 +6,175 @@ contract VickeryAuction{
     uint32 commitmentPhaseLength;
     uint32 withdrawalPhaseLength;
     uint32 openingPahseLength;
-    uint256 reservePrice;
+    uint256 public reservePrice;
     uint256 public depositRequired;
-    address owner;
+    address seller; // he deploys the contract to sell something he owns
     mapping(address => uint256) commitedEnvelops; 
+
+    uint256 gracePeriod;
     
-    uint256 bestBid;
-    address bestBidAddress;
+    uint256 firstBid;
+    address firstBidAddress;
     uint256 secondBid;
     address secondBidAddress;
     
-    function VickeryAuction (uint256  _reservePrice,
+    // events
+    event CommitedEnvelop(address bidderAddress);
+    event Withdraw(address leavingBidderAddress);
+    event Open(address bidderAddress, uint256 value);
+    event FirstBid(address bidderAddress, uint256 value);
+    event SecondBid(address bidderAddress, uint256 value);
+    event Winner(address winnerBidder);
+    
+    // testing related evetnts
+    event NewBlock(uint256 blockNum);
+    event Hash(bytes32 h); 
+    
+    
+    constructor (uint256  _reservePrice,
                 uint32 _commitmentPhaseLength,
                 uint32 _withdrawalPhaseLength,
                 uint32 _openingPahseLength,
-                uint256 _depositRequired)public {
-        // getting the address of the contract creator
-        owner = msg.sender;
-        reservePrice = _reservePrice;
-        commitmentPhaseLength = _commitmentPhaseLength;
-        withdrawalPhaseLength = _withdrawalPhaseLength;
-        openingPahseLength = _openingPahseLength;
-
-        depositRequired = _depositRequired;    
-        
-        secondBid = bestBid = reservePrice -1; // TODO check this better
-        secondBidAddress = bestBidAddress = this;
+                uint256 _depositRequired,
+                uint32 miningRate)public {
+            // getting the address of the contract creator
+            seller = msg.sender;
+            reservePrice = _reservePrice;
+            commitmentPhaseLength = _commitmentPhaseLength;
+            withdrawalPhaseLength = _withdrawalPhaseLength;
+            openingPahseLength = _openingPahseLength;
+    
+            depositRequired = _depositRequired;    
+            
+            // miningRate == 15 means that on average one block is mined every 15 seconds
+            gracePeriod = block.number + 5*60 / miningRate; 
+            
+            /*
+            * Since the first bid is init to reservePrice the bidder to win has to al least offer reservePrice + 1
+            * The semantic is not perfect, but keeping it in this way allows the code to be more clean
+            */
+            secondBid = firstBid = reservePrice; 
+            secondBidAddress = firstBidAddress = this;
         }
-        // TODO check that the owner does not call the functions
-        function commitBid(uint256 envelop) external payable{ 
-            // need a modifier to check time validity
-            // won't keeep the actual deposit given cuse its gas consuming
-            // the sender should send the right ammount 
-            require(msg.value >= depositRequired);
+
+
+        modifier checkCommitmentPahseLenght(){
+            require(gracePeriod < block.number && block.number <= gracePeriod + commitmentPhaseLength);_;
+        }
+        modifier checkWithdrawalPhaseLength(){
+            require(gracePeriod + commitmentPhaseLength < block.number && block.number <= gracePeriod + commitmentPhaseLength + withdrawalPhaseLength);_;
+        }
+        modifier checkOpeningPahseLength(){
+            require(gracePeriod + commitmentPhaseLength + withdrawalPhaseLength < block.number &&
+                        block.number <= gracePeriod + commitmentPhaseLength + withdrawalPhaseLength + openingPahseLength);_;
+        }
+        modifier checkAuctionEnd(){
+            require(gracePeriod + commitmentPhaseLength + withdrawalPhaseLength + openingPahseLength < block.number);_;
+        }
+        
+ 
+        function commitBid( uint256 envelop) external payable checkCommitmentPahseLenght(){ 
+            // the seller can't bid
+            require(msg.sender != seller);
+            // won't keeep the actual deposit given cuse its gas consuming,  the sender should send the right ammount 
+            require(msg.value >= depositRequired );
+            // the bidder can't bid more than one time
+            require(commitedEnvelops[msg.sender] == 0);
             
             commitedEnvelops[msg.sender] = envelop;
+            emit CommitedEnvelop(msg.sender);
         }
         
-        function withdraw() external {
-            // need a modifier to check time validity
+        
+        function withdraw() external checkWithdrawalPhaseLength(){
+
             if (commitedEnvelops[msg.sender] != 0){
                 // this avoids to call the function multiple times
                 commitedEnvelops[msg.sender] = 0;
                 msg.sender.transfer(depositRequired/2);
+                emit Withdraw(msg.sender);
             }
         }
         
-        function open(uint32 nonce) external payable{
-            // need a modifier to check time validity
+        
+        function open(uint256 nonce) external payable checkOpeningPahseLength(){
             // checking if the bid and the evelop match
             uint256 tmphash = uint256(keccak256(msg.value, nonce));
+            // the last condition is useful to a avoid a fake withdraw: a bidder can bid 0 then open to get the deposit back 
             require((commitedEnvelops[msg.sender] == tmphash) && (msg.value >= reservePrice) );
             
-            if(msg.value > bestBid){
-                if (bestBid != reservePrice-1) // refounding
-                    bestBidAddress.transfer(bestBid + depositRequired); // check transfer (transfer aborts all and rolls back) 
-                    // send instead reutrn false if it is unsuccessful
-                bestBid = msg.value;
-                bestBidAddress = msg.sender;
+            // to avoid more calls of the function from the same bidder
+            commitedEnvelops[msg.sender] = 0;
+            emit Open(msg.sender, msg.value);
+            
+            if(msg.value > firstBid){
+                if (firstBid != reservePrice) // refounding
+                    firstBidAddress.transfer(firstBid + depositRequired); 
+                    
+                firstBid = msg.value;
+                firstBidAddress = msg.sender;
+                emit FirstBid(msg.sender, msg.value);
+                
             }else if (msg.value > secondBid) {
-                if (secondBid != reservePrice-1) 
-                    secondBidAddress.transfer(bestBid + depositRequired);
+                if (secondBid != reservePrice) 
+                    secondBidAddress.transfer(firstBid + depositRequired);
+                
                 secondBid = msg.value;
                 secondBidAddress = msg.sender;
+                emit SecondBid(msg.sender, msg.value);
+            
             }else
                 msg.sender.transfer(msg.value + depositRequired);
         }
         
-        function finalize() external {
-            // need a modifier to check time validity
-            require(msg.sender == owner);
+        
+        function finalize() external checkAuctionEnd(){
+            // only the seller can call this
+            require(msg.sender == seller);
             
-            if (secondBid == reservePrice-1) // TDO check this I don't like it
-                secondBid = reservePrice;
-            else{
-                // sending back ether to second winner
-                secondBidAddress.send(secondBid + depositRequired);
-            }
+            if (secondBid != reservePrice)
+                secondBidAddress.transfer(secondBid + depositRequired);
+            
             // the winner pays the ammount offered by the second winner
-            bestBidAddress.transfer(bestBid - secondBid + depositRequired);
+            emit Winner(firstBidAddress);
+            firstBidAddress.transfer(firstBid - secondBid + depositRequired);
             
             // TODO implement escrow??
             
-            owner.transfer(secondBid);
+            seller.transfer(secondBid);
+            
+            //burning remaining ether
+            address burnAddress = 0x0;
+            burnAddress.transfer(address(this).balance);
         }
+        
+        
+        /*
+        * The function above are added only to test better the contract.
+        * In a real environment they should be removed
+        */
+        function addBlock() public  payable{
+            emit NewBlock(block.number);
+        }
+        
+        /*
+        * Can be used to retrive the hash to be passed to the open function
+        */
+        function doKeccak(uint256 value, uint256 nonce) external {
+            emit Hash(keccak256(value,nonce));
+        }
+        
+        
 }
+
+
+
+
+
+
+
+
+
 
 
 
